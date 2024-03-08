e37bc5e5399eea2b0694d49e909ec6600fae0f4a
Ananta <anantahatwar1@gmail.com>
2024-03-08 20:42:28 +0530

    adding login pages

---
+++ client/src/components/Guest.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Admin.css'; // Using the same CSS file for styling
+
+const GuestLogin = () => {
+  return (
+    <div className="all">
+        <div className="login-container guest">
+        <h2>Guest Login</h2>
+        <form>
+            <div className="form-group">
+            <i className="icon-email">📧</i> {/* Placeholder icon */}
+            <input type="email" placeholder="Email" required />
+            </div>
+            <div className="form-group">
+            <i className="icon-password">🔒</i> {/* Placeholder icon */}
+            <input type="password" placeholder="Password" required />
+            </div>
+            <button type="submit">Login</button>
+        </form>
+        </div>
+    </div>
+        
+  );
+};
+
+export default GuestLogin;

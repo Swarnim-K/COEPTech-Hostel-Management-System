af1fa454de3a7b93faff1073907895f9881cab10
Ananta <anantahatwar1@gmail.com>
2024-03-30 19:45:26 +0530

    -l

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import GuestLogin from './components/Guest.jsx';
 import Home from './pages/Home.jsx';
 import FeatureComponent from './components/FeatureComponent'
 import StudentForm from './components/StudentForm.jsx';
+import RoomDisplay from './components/Roomdisplay.jsx';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/guest-login" element={<GuestLogin />} />
         <Route path="/admin-info" element={<FeatureComponent />} />
         <Route path="/admit-student" element={<StudentForm />} />
+        <Route path="//room-data" element={<RoomDisplay />} />
       </Routes>
     </BrowserRouter>
   );
